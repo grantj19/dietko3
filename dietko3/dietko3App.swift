b4b0419da35497998434b7bf6ffbbdac9aed5987
grantgillespie <eaglegolfer@outlook.com>
2023-09-19 22:09:09 -0400

    Initial Commit

---
+++ dietko3/dietko3App.swift
@@ -0,0 +1,17 @@
+//
+//  dietko3App.swift
+//  dietko3
+//
+//  Created by Grant Gillespie on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct dietko3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
